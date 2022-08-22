076b92a6229e01e9047c07e47836369f2383676a
Juan Marin <jcmarinvelez@gmail.com>
2022-08-22 16:15:05 -0500

    applied new view functionality

--- tasks.js
+++ tasks.js
@@ -1,81 +1,136 @@
-const taskInput = document.getElementById('task');
-const taskList = document.getElementById('taskList');
+let tasksArray = []; // Colección de tareas
+// Seleccionar el elemento del DOM
+const taskInput = document.getElementById("task");
+const tasksList = document.getElementById("tasksList");
 
-class Tasks {
-
-  constructor() {}
-  #taskList = [];
-
-  getTasks() {
-    //Code here
+taskInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addTask();
   }
+});
 
-  getTask(id) {
-    //Code here
+// Mostrar notificaciones
+function showNotification(type, message) {
+  alertify.set("notifier", "position", "top-right");
+  alertify.notify(message, type);
+}
+
+// Verificar si la tarea ya existe
+function taskExists(activity) {
+  const result = tasksArray.filter((task) => task.activity === activity);
+  if (result.length) {
+    showNotification("error", "La tarea ya existe");
+    return false;
   }
+  return true;
+}
 
-  addTask() {
-    const task = taskInput.value;
+// Agregar una tarea
+function addTask() {
+  const task = taskInput.value;
 
-    if(!task){
-        return
-    }
-    
-    this.#taskList.push({
-        id:  this.#taskList.length,
-        activity: task,
-        done: false
+  if (task && taskExists(task)) {
+    tasksArray.push({
+      id: tasksArray.length, // Tamaño del array
+      activity: task,
+      done: false,
+    }); // agregar tarea a la colección de tareas
+    taskInput.value = ""; // Limpiar el input
+    taskInput.placeholder = "¿Qué quieres hacer?"; // Cambiar el placeholder a la versión original
+    taskInput.classList.remove("inputText--error"); // Eliminar la clase de error
+    showTasks();
+  } else {
+    taskInput.focus(); // Focus en el input
+    taskInput.classList.add("inputText--error"); // Agregar clase de error
+    taskInput.placeholder = "¡Agrega una tarea!"; // Cambiar el placeholder a la versión de error
+  }
+}
+
+// Marcar una tarea como hecha
+function finishTask(status, id) {
+  if (!status) {
+    const index = tasksArray.findIndex((value) => {
+      return value.id === id;
     });
-    taskInput.value = '';
-
-   this.showTasks()
+    tasksArray[index].done = true;
+    showTasks();
   }
+}
 
-  editTask(status, id) {
-    if (!status) {
-      const index =  this.#taskList.findIndex((value) => {
-        return value.id === id;
-      });
-      this.#taskList[index].done = true;
-      this.showTasks()
-    }
-  }
+// Eliminar una tarea
+function deleteTask(id) {
+  /*const index = tasksArray.findIndex((value) => {
+    return value.id === id;
+  });
+  tasksArray.splice(index, 1);
+  showTasks();*/
+  tasksArray = tasksArray.filter((task) => task.id !== id);
+  showTasks();
+}
 
-  deleteTask(id){
+// Editar una tarea
+function editTask(id) {
+  const index = tasksArray.findIndex((value) => {
+    return value.id === id;
+  });
+  const activity = tasksArray[index].activity;
+  alertify
+    .prompt(
+      "Editar tarea",
+      "¿Qué quieres hacer?",
+      activity,
+      function (evt, value) {
+        if ((value && value === activity) || !value) {
+          showNotification("error", "No se ha editado la tarea");
+          return false;
+        }
 
-     this.#taskList =  this.#taskList.filter(t => t.id != id )
+        if (!taskExists(value)) {
+          return false;
+        }
 
-    this.showTasks()
+        tasksArray[index].activity = value;
+        showNotification("success", "Actividad editada");
+        showTasks();
+      },
+      null
+    )
+    .set("labels", { ok: "Editar", cancel: "Cancelar" });
+}
 
-  }
+// Mostrar las tareas
+function showTasks() {
+  tasksList.classList.remove("tasksList");
+  tasksList.innerHTML = ""; // Limpiar el contenido del elemento
 
-  showTasks(){
-    taskList.innerHTML = ""; // Limpiar el contenido del elemento
-
-  if ( this.#taskList.length) {
-    taskList.setAttribute("class", "tasksList"); // Agregar clase al elemento
-     this.#taskList
+  if (tasksArray.length) {
+    tasksList.setAttribute("class", "tasksList"); // Agregar clase al elemento
+    tasksArray
       .sort((a, b) => a.done - b.done) // Ordenar las tareas por estado (hecha o no hecha)
       .forEach((value) => {
         // Recorrer el array de tareas
         const li = document.createElement("li");
         // string templates
         li.innerHTML = ` 
-      <span class="${value.done ? "tasksItemDone" : ""}">${
+      <span class="tasksItem${value.done ? " tasksItem--done" : ""}">${
           value.activity
         }</span>
       <span class="tasksItemAction">
-        <i class="fa fa-check ${
+      
+        <i class="fa fa-check tasksItemAction-done ${
           value.done ? "hidden" : "" // Si la tarea está hecha, no mostrar el icono
-        }"  onclick='tasks.editTask(${value.done}, ${value.id})'></i>
-        <i class="fa fa-trash-o" onclick='tasks.deleteTask(${value.id})'></i>
+        }"  onclick='finishTask(${value.done}, ${value.id})'></i>
+        <i class="fa fa-edit tasksItemAction-edit  ${
+          value.done ? "hidden" : ""
+        }" onclick='editTask(${value.id})'></i>
+        <i class="fa fa-trash-o tasksItemAction-delete" onclick='deleteTask(${
+          value.id
+        })'></i>
       </span>
       `;
-        taskList.appendChild(li); // Agregar el elemento al DOM
+        tasksList.appendChild(li); // Agregar el elemento al DOM
       });
-  }
-     
-}
-}
 
-let tasks = new Tasks();
+    taskInput.focus();
+  }
+}
--- tasks.view.js
+++ tasks.view.js
@@ -1,52 +1,171 @@
-import Tasks from "./tasks.class.js"
-(function(Tasks) {
+// Importamos la clase Tasks de tasks/Tasks.class.js:
+import Tasks from "./Tasks.class.js";
 
-    const tasks = new Tasks();
+//Creamos una función auto executable para inicializar la clase Tasks
+(function (Tasks) {
+  // Creamos una instancia de la clase Tasks
+  const tasks = new Tasks();
 
-    const taskInput = document.getElementById('task');
-    const taskList = document.getElementById('taskList');
-    const addTaskButton = document.getElementById('addTask');
+  const taskInput = document.getElementById("task");
+  const tasksList = document.getElementById("tasksList");
+  const addTaskButton = document.getElementById("addTask");
 
-    
-
-    taskInput.addEventListener("keydown",function(event){
-
-        if(event.key === "Enter"){
-            addTask()
-        }
-    })
-
-
-    addTaskButton.addEventListener("click", addTask)
-
-    function addTask () {
-        const task = taskInput.value;
-        if (task && taskExists(task)) {
-            tasksArray.push({
-              id: tasksArray.length, // Tamaño del array
-              activity: task,
-              done: false,
-            }); // agregar tarea a la colección de tareas
-            taskInput.value = ""; // Limpiar el input
-            taskInput.placeholder = "¿Qué quieres hacer?"; // Cambiar el placeholder a la versión original
-            taskInput.classList.remove("inputText--error"); // Eliminar la clase de error
-        } else {
-          taskInput.focus(); // Focus en el input
-          taskInput.classList.add("inputText--error"); // Agregar clase de error
-          taskInput.placeholder = "¡Agrega una tarea!"; // Cambiar el placeholder a la versión de error
-        }
+  // Agregar un evento al input de tareas
+  taskInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      addTask();
     }
+  });
 
-    // Verificar si la tarea ya existe
-    function taskExists(activity) {
-        const result = tasksArray.filter((task) => task.activity === activity);
-        if (result.length) {
-        showNotification("error", "La tarea ya existe");
-        return false;
-        }
-        return true;
+  // Agregar un evento al botón de agregar tarea
+  addTaskButton.addEventListener("click", addTask);
+
+  // Agregar eventos a los iconos de las tareas
+  function addEventsToIcons(id) {
+    const finishTaskButton = document.getElementById("finishTask-" + id);
+    const editTaskButton = document.getElementById("editTask-" + id);
+    const deleteTaskButton = document.getElementById("deleteTask-" + id);
+
+    finishTaskButton.addEventListener("click", function (event) {
+      const status = event.target.getAttribute("data-status") === "true";
+      const id = event.target.getAttribute("data-id");
+      finishTask(status, Number(id));
+    });
+
+    editTaskButton.addEventListener("click", function (event) {
+      const id = event.target.getAttribute("data-id");
+      editTask(Number(id));
+    });
+
+    deleteTaskButton.addEventListener("click", function (event) {
+      const id = event.target.getAttribute("data-id");
+      deleteTask(Number(id));
+    });
+  }
+
+  // Agregar una tarea
+  function addTask() {
+    const task = taskInput.value;
+    if (task && taskExists(task)) {
+      tasks.addTask(task);
+      taskInput.value = ""; // Limpiar el input
+      taskInput.placeholder = "¿Qué quieres hacer?"; // Cambiar el placeholder a la versión original
+      taskInput.classList.remove("inputText--error"); // Eliminar la clase de error
+      showTasks();
+    } else {
+      taskInput.focus(); // Focus en el input
+      taskInput.classList.add("inputText--error"); // Agregar clase de error
+      taskInput.placeholder = "¡Agrega una tarea!"; // Cambiar el placeholder a la versión de error
     }
+  }
 
-    console.log(Tasks)
+  // Marcar una tarea como hecha
+  function finishTask(status, id) {
+    const taskList = tasks.getTasks();
+    if (!status) {
+      const index = taskList.findIndex((value) => {
+        return value.id === id;
+      });
+      tasks.editTask(index, "done", true);
+      showTasks();
+    }
+  }
 
-} )(Tasks)
+  // Eliminar una tarea
+  function deleteTask(id) {
+    tasks.deleteTask(id);
+    showTasks();
+  }
+
+  // Editar una tarea
+  function editTask(id) {
+    const taskList = tasks.getTasks();
+    const index = taskList.findIndex((value) => {
+      return value.id === id;
+    });
+    const activity = taskList[index].activity;
+    alertify
+      .prompt(
+        "Editar tarea",
+        "¿Qué quieres hacer?",
+        activity,
+        function (evt, value) {
+          if ((value && value === activity) || !value) {
+            showNotification("error", "No se ha editado la tarea");
+            return false;
+          }
+
+          if (!taskExists(value)) {
+            return false;
+          }
+
+          tasks.editTask(id, value);
+          showNotification("success", "Actividad editada");
+          showTasks();
+        },
+        null
+      )
+      .set("labels", { ok: "Editar", cancel: "Cancelar" });
+  }
+
+  // Mostrar las tareas
+  function showTasks() {
+    const data = tasks.getTasks();
+    tasksList.innerHTML = ""; // Limpiar el contenido de la lista de tareas
+    tasksList.classList.remove("tasksList");
+    tasksList.innerHTML = ""; // Limpiar el contenido del elemento
+
+    if (data.length) {
+      tasksList.setAttribute("class", "tasksList"); // Agregar clase al elemento
+      data
+        .sort((a, b) => a.done - b.done) // Ordenar las tareas por estado (hecha o no hecha)
+        .forEach((value) => {
+          // Recorrer el array de tareas
+          const li = document.createElement("li");
+          // string templates
+          li.innerHTML = ` 
+      <span class="tasksItem${value.done ? " tasksItem--done" : ""}">
+        ${value.activity}
+      </span>
+      <span class="tasksItemAction">
+        <i id="finishTask-${value.id}" 
+        class="fa fa-check tasksItemAction-done ${value.done ? "hidden" : ""}"
+        data-id="${value.id}" 
+        data-status="${value.done}">
+        </i>
+        <i id="editTask-${value.id}" 
+        class="fa fa-edit tasksItemAction-edit ${value.done ? "hidden" : ""}" 
+        data-id="${value.id}">
+        </i>
+        <i id="deleteTask-${value.id}" 
+        class="fa fa-trash-o tasksItemAction-delete"
+        data-id='${value.id}'>
+        </i>
+      </span>
+      `;
+          tasksList.appendChild(li); // Agregar el elemento al DOM
+          // Agregar eventos a los iconos de las tareas dinámicamente
+          addEventsToIcons(value.id);
+        });
+
+      taskInput.focus();
+    }
+  }
+
+  // Verificar si la tarea ya existe
+  function taskExists(activity) {
+    const taskList = tasks.getTasks();
+    const result = taskList.filter((task) => task.activity === activity);
+    if (result.length) {
+      showNotification("error", "La tarea ya existe");
+      return false;
+    }
+    return true;
+  }
+
+  // Mostrar notificaciones
+  function showNotification(type, message) {
+    alertify.set("notifier", "position", "top-right");
+    alertify.notify(message, type);
+  }
+})(Tasks);
